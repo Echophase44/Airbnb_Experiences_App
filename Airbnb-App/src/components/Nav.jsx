36660c3b509eb378908c161430e2f5c072fbba75
Nick <Echophase44@github.com>
2022-11-07 06:32:28 -0500

    Added a couple images files and completed Navbar section

---
+++ Airbnb-App/src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Logo from "../assets/airbnb.png"
+
+function Nav() {
+  return (
+    <nav className="nav-bar">
+      <img src= { Logo } alt="Airbnb Logo" />
+    </nav>
+  )
+}
+
+export default Nav;
